6d7419317e085fb3367850a57f938e2cf2592eaf
Mauro Worobiej <mauro.worobiej@gmail.com>
2022-10-12 09:38:29 -0300

    Create the FeedLoader protocol boundary

---
+++ EssentialFeed/EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Mauro Worobiej on 12/10/2022.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
