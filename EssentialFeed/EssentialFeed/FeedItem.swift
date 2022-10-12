5fb7644cdda028270e91a4544b1e024f68d44665
Mauro Worobiej <mauro.worobiej@gmail.com>
2022-10-12 09:38:02 -0300

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mauro Worobiej on 12/10/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
